5bca572b929d426286c4045c9dbc485e44b57442
Alina Skripkiene <alinaskripkiene@Alinas-MacBook-Pro.local>
2016-08-21 16:34:37 +0300

    parse urls

--- link-checker.go
+++ link-checker.go
@@ -9,6 +9,7 @@ import (
 	"io/ioutil"
 	"unicode/utf8"
 	//"time"
+	"regexp"
 )
 
 type Result struct {
@@ -40,7 +41,6 @@ func checkUrls(urls []string) (<-chan Result, <-chan bool) {
   		}
   		for i := 0; i < len(urls); i++ { //wait till all urls all hecked
   			<-internalFinishChan
-  			fmt.Println("Single Url finished")
   		}
   		finishChan <- true
 	}()
@@ -53,15 +53,12 @@ func checkUrl(url string, resultChan chan Result, finishChan chan bool) {
 
 
 	go func() {
-		fmt.Println("Start")
 		//	r, err := http.Get("https://www.transfergo.com/lt")
 		r, err := http.Get(url)
 		if err != nil {
 			log.Fatal(err)
 			fmt.Println("Error")
 		} else {
-			fmt.Println("Success")
-			
 			defer r.Body.Close()
 
 			body, err := ioutil.ReadAll(r.Body)
@@ -70,21 +67,28 @@ func checkUrl(url string, resultChan chan Result, finishChan chan bool) {
 				fmt.Println(err)
 			} else {
 				stringBody := fmt.Sprintf("%s", body)
-				fmt.Println(utf8.RuneCountInString(stringBody))
-				//fmt.Println(stringBody)		
-			}
-			//fmt.Println(reflect.TypeOf(r.Body))
-			//fmt.Printf("%#v", r.Body)
-			//io.Copy(os.Stdout, r.Body)
-	//		fmt.Println(r.Body)
-	//	    fmt.Println("hello world")	
-			fmt.Println("Success")
-			fmt.Println(url)
+				utf8.RuneCountInString(stringBody)
+				//fmt.Println(stringBody)
+
+				newUrls := findUrls(stringBody)
+				for _,newUrl := range newUrls { //initialize checking of all URLs
+
+					fmt.Println(newUrl[1])
+  				}
+  			}
+			fmt.Println(url + " Succeeded")
 		}
 
 		//res := new(Result)
 		finishChan <- true
 	}()
+}
+
+func findUrls(html string) [][]string {
+	re := regexp.MustCompile("<a .* href=\"(https://www.tgstatic.com/[^\"]*)")
+	// fmt.Println(re.FindAllString("paranormal", -1))
+
+	return re.FindAllStringSubmatch(html, -1)
 }
 
 func main() {
@@ -95,5 +99,8 @@ func main() {
 		result := <-resultChan
 		fmt.Println(result)
 	}()
+
+	
+
 	<-finishChan
 }
