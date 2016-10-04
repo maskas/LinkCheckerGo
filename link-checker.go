7094a115bdc7d31e5cb26d2e536eb9ddf64e5fc1
Mangirdas Skripka <mangirdas@transfergo.com>
2016-10-04 15:55:38 +0300

    include css

--- link-checker.go
+++ link-checker.go
@@ -52,13 +52,19 @@ func checkUrl(url string, source string, resultChan chan Result) {
 func findUrls(html string) []string {
 	var urls = []string{}
 	
-	re := regexp.MustCompile("<a .* href=\"([^\"]*)")
+	re := regexp.MustCompile("<a .*href=\"([^\"]*)")
 	matches := re.FindAllStringSubmatch(html, -1)
 	for _,match := range matches {
 		urls = append(urls, match[1])
 	}
 
-	re = regexp.MustCompile("<img .* src=\"([^\"]*)")
+	re = regexp.MustCompile("<img .*src=\"([^\"]*)")
+	matches = re.FindAllStringSubmatch(html, -1)
+	for _,match := range matches {
+		urls = append(urls, match[1])
+	}
+
+	re = regexp.MustCompile("<link .*href=\"([^\"]*)")
 	matches = re.FindAllStringSubmatch(html, -1)
 	for _,match := range matches {
 		urls = append(urls, match[1])
